c9a684550809bfe91da0d1e3de51bdee92c3c18e
Anastasiia Klykova <qwerty03031998@yandex.ru>
2023-06-20 21:44:23 +0300

    Испр. ошибки после автотестов

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 const router = require('express').Router();
 const { auth } = require('../middlewares/auth');
 const { login, createUser } = require('../controllers/users');
-const NotFound = require('../errors/NotFound');
 const { signInValidation, signUpValidation } = require('../utils/validation');
-
-router.use('/users', auth, require('./users'));
-router.use('/cards', auth, require('./cards'));
+const NotFound = require('../errors/NotFound');
 
 router.use('/signin', signInValidation, login);
 router.use('/signup', signUpValidation, createUser);
+
+router.use(auth);
+
+router.use('/users', auth, require('./users'));
+router.use('/cards', auth, require('./cards'));
 
 router.use('/*', (req, res, next) => {
   next(new NotFound('Такой ссылки не существует'));
